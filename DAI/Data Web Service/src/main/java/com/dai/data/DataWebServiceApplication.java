d4504cdea9fd292f6ea69fbd346eadf557e2bdd1
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-03-26 14:25:37 +0100

    Creating simple controller for circle ci testing purposes

--- DAI/Data Web Service/src/main/java/com/dai/data/DataWebServiceApplication.java
+++ DAI/Data Web Service/src/main/java/com/dai/data/DataWebServiceApplication.java
@@ -2,12 +2,19 @@ package com.dai.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DataWebServiceApplication {
+
+    @GetMapping("/")
+    public String getMessage() {
+        return "Rest API test for docker";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DataWebServiceApplication.class, args);
     }
-
 }
