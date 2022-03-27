14daa86a5ea322c691db1aeae6b012c23faab8bc
Geana <ion.georgiana@hotmail.com>
2022-03-27 11:59:07 +0200

    First try in creating the drawer, needs fixing

--- AND/app/src/main/java/com/example/farmerama/MainActivity.java
+++ AND/app/src/main/java/com/example/farmerama/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.farmerama;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    NavController navController;
+    AppBarConfiguration appBarConfiguration;
+    DrawerLayout drawerLayout;
+    NavigationView navigationDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_activity_main);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initViews();
+
+        setupNavigation();
+    }
+
+    private void initViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationDrawer = findViewById(R.id.nav_view);
+    }
+
+    private void setupNavigation() {
+        navController = Navigation.findNavController(this, R.id.fragment);
+        setSupportActionBar(toolbar);
+
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment,
+                R.id.accountFragment)
+                .setOpenableLayout(drawerLayout)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationDrawer, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 }
---
+++ AND/app/src/main/java/com/example/farmerama/SensorsFragment.java
@@ -0,0 +1,21 @@
+package com.example.farmerama;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SensorsFragment extends Fragment {
+
+    public SensorsFragment(){}
+
+ @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sensors, container, false);
+    }
+}
