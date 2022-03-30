914c0df3a81b31ce2a06052e404885def29faa61
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-03-30 09:19:23 +0200

    CD trigger

--- DAI/Data Web Service/src/main/java/com/dai/data/DataWebServiceApplication.java
+++ DAI/Data Web Service/src/main/java/com/dai/data/DataWebServiceApplication.java
@@ -2,7 +2,9 @@ package com.dai.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
