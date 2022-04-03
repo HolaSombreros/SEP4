b6d84cc4eee9f79ef196610408b1ee43e7c3a57d
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-04-03 11:37:26 +0200

    Controller fix

--- DAI/Data Web Service/src/main/java/com/dai/DataWebServiceApplication.java
+++ DAI/Data Web Service/src/main/java/com/dai/DataWebServiceApplication.java
@@ -2,6 +2,10 @@ package com.dai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
