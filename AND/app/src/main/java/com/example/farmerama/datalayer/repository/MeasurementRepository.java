9861a0bb7df96238dabbad2b5846ca6e4b67c060
Tabita <304181@via.dk>
2022-04-04 10:33:09 +0200

    Login view and view model impl

--- AND/app/src/main/java/com/example/farmerama/datalayer/repository/MeasurementRepository.java
+++ AND/app/src/main/java/com/example/farmerama/datalayer/repository/MeasurementRepository.java
@@ -1,18 +1,27 @@
 package com.example.farmerama.datalayer.repository;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.farmerama.datalayer.dao.MeasurementDAO;
 import com.example.farmerama.datalayer.model.Measurement;
+import com.example.farmerama.datalayer.network.MeasurementApi;
+import com.example.farmerama.datalayer.network.ServiceGenerator;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.internal.EverythingIsNonNull;
 
 public class MeasurementRepository {
 
     private MeasurementDAO measurementDAO;
     private static MeasurementRepository instance;
     private static Object lock = new Object();
-    private static MeasurementRepository instance;
     //private MutableLiveData<List<Measurement>> measurements;
     private MutableLiveData<Measurement> measurement;
 
@@ -21,28 +30,11 @@ public class MeasurementRepository {
         measurement = new MutableLiveData<>();
     }
 
-    public MeasurementRepository() {
-        measurementDAO = MeasurementDAO.getInstance();
-    }
-
     public static MeasurementRepository getInstance() {
         if(instance == null){
             synchronized (lock) {
                 instance = new MeasurementRepository();
             }
-        }
-        return instance;
-    }
-    /**
-     * List of latest measurement of temperature, humidity and so on
-     * @param areaId
-     * @return
-     */
-    public MutableLiveData<List<Measurement>> getLatestMeasurement(int areaId) {
-        return measurementDAO.getLatestMeasurements(areaId);
-    public static synchronized MeasurementRepository getInstance() {
-        if (instance == null) {
-            instance = new MeasurementRepository();
         }
         return instance;
     }
