5af4015856db1c27a8060969b40d7207a589b76a
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-04-06 13:31:43 +0200

    User repo implementation

--- DAI/Data Web Service/src/main/java/com/dai/repository/UserRepository.java
+++ DAI/Data Web Service/src/main/java/com/dai/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.dai.repository;
 
 import com.dai.shared.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
-    User getUserByEmailAndPassword(String email, String password);
+    User findFirstByEmail(String email);
+    User deleteById(int id);
+    List<User> findAll();
 }
