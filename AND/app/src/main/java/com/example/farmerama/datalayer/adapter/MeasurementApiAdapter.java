abc198068067ead2940f4202cb9aa023b1db5e42
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2022-05-04 13:27:30 +0200

    Measurement Adapter

--- AND/app/src/main/java/com/example/farmerama/datalayer/adapter/MeasurementApiAdapter.java
+++ AND/app/src/main/java/com/example/farmerama/datalayer/adapter/MeasurementApiAdapter.java
@@ -1,10 +1,10 @@
 package com.example.farmerama.datalayer.adapter;
 
-import com.example.farmerama.datalayer.model.Measurement;
 import com.example.farmerama.datalayer.model.MeasurementType;
+import com.example.farmerama.datalayer.model.response.MeasurementResponse;
 
 import retrofit2.Call;
 
 public interface MeasurementApiAdapter {
-    Call<Measurement> retrieveLatestMeasurement(MeasurementType type, int areaId);
+    Call<MeasurementResponse> retrieveLatestMeasurement(MeasurementType type, int areaId, boolean latest);
 }
