5f60b906057e515a99455344be93949362f67b11
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-05-07 14:06:10 +0200

    Fail check

--- DAI/Data Web Service/src/main/java/com/dai/etl/Etl.java
+++ DAI/Data Web Service/src/main/java/com/dai/etl/Etl.java
@@ -2,6 +2,8 @@ package com.dai.etl;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -25,12 +27,15 @@ public class Etl {
     //This script is being run each day at midnight
     //TODO uncomment when the script will be correct
 //    @Scheduled(cron = "0 0 0 ? * *")
+    @Scheduled(fixedDelay = 10000)
+    @Async
     public void performEtl() {
         try {
             Connection c = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
             if (c == null)
                 return;
-
+            System.out.println(dbUsername);
+            System.out.println(dbPassword);
             Reader reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("Etl.sql")));
             ScriptRunner sr = new ScriptRunner(c);
             sr.runScript(reader);
