b81b42d7454c506cca90bb446a7ab1637cd5376d
Sombrero <adriana@grecea.net>
2022-05-13 11:59:26 +0200

    get humidity measurements for a specific day

--- DAI/Data Web Service/src/main/java/com/dai/controllers/HumidityController.java
+++ DAI/Data Web Service/src/main/java/com/dai/controllers/HumidityController.java
@@ -5,8 +5,11 @@ import com.dai.model.humidity.HumidityModel;
 import com.dai.shared.SentMeasurement;
 import com.dai.shared.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,7 +24,7 @@ public class HumidityController {
         this.humidityModel = humidityModel;
     }
 
-    @GetMapping("/areas/{id}/humidities")
+    @GetMapping(value = "/areas/{id}/humidities", params = "latest")
     public List<SentMeasurement> readLastHumidity(@PathVariable("id") int areaId, @RequestParam("latest") Optional<Boolean> isLatest) {
         try {
             if (isLatest.isPresent() && isLatest.get()) {
@@ -33,4 +36,15 @@ public class HumidityController {
             throw new BadRequestException(e.getMessage());
         }
     }
+
+    @GetMapping(value = "/areas/{id}/humidities", params = "date")
+    public List<SentMeasurement> getAllMeasurementsByDate(@PathVariable("id") int areaId, @Valid @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Optional<LocalDate> localDate){
+        try{
+            return humidityModel.getHumidityMeasurementsByDate(areaId, localDate.get());
+        }catch (Exception e) {
+            throw new BadRequestException(e.getMessage());
+        }
+
+    }
+
 }
