3f66fff08d8ae57d7fdba743bc0dfb40a3345918
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-05-13 12:01:34 +0200

    Feature - Read all temperatures and get temperatures in a specific day

--- DAI/Data Web Service/src/main/java/com/dai/model/temperature/TemperatureModel.java
+++ DAI/Data Web Service/src/main/java/com/dai/model/temperature/TemperatureModel.java
@@ -2,8 +2,11 @@ package com.dai.model.temperature;
 
 import com.dai.shared.SentMeasurement;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TemperatureModel {
     List<SentMeasurement> readLatestTemperature(int areaId) throws Exception;
+    List<SentMeasurement> readAllTemperatures(int id) throws Exception;
+    List<SentMeasurement> getAreaTemperaturesByDate(int areaId, LocalDate date) throws Exception;
 }
