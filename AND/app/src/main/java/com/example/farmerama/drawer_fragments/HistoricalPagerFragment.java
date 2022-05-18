196a71d4992b1565f46c762bb12b775855c9b415
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2022-05-18 09:36:24 +0200

    Merge conflicts

--- AND/app/src/main/java/com/example/farmerama/drawer_fragments/HistoricalPagerFragment.java
+++ AND/app/src/main/java/com/example/farmerama/drawer_fragments/HistoricalPagerFragment.java
@@ -31,7 +31,7 @@ public class HistoricalPagerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_historical_pager, container, false);
+        return inflater.inflate(R.layout.fragment_historical_data, container, false);
     }
 
 
