9507cc01b333bc018c84316639d10d7410472296
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-05-25 16:06:05 +0200

    Correcting validator values

--- DAI/Data Web Service/src/main/java/com/dai/helpers/MeasurementValidator.java
+++ DAI/Data Web Service/src/main/java/com/dai/helpers/MeasurementValidator.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class MeasurementValidator {
 
     public boolean isTemperatureValueValid(double value) {
-        return value >= 0 && value <= 60;
+        return value >= -20 && value <= 60;
     }
 
     public boolean isHumidityValueValid(double value) {
@@ -14,10 +14,10 @@ public class MeasurementValidator {
     }
 
     public boolean isCo2ValueValid(int value) {
-        return value >= 150 && value <= 3000;
+        return value >= 50 && value <= 3000;
     }
 
     public boolean isSoundValueValid(int value) {
-        return value >= 0 && value <= 120;
+        return value >= 0 && value <= 150;
     }
 }
