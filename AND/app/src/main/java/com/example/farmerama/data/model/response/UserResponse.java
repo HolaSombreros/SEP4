52afe087e88ca2899c43895734f6770a5bcf05cf
Tabita-Roxana <tabita.varlan@gmail.com>
2022-05-27 13:44:18 +0200

    account + edit account views

--- AND/app/src/main/java/com/example/farmerama/data/model/response/UserResponse.java
+++ AND/app/src/main/java/com/example/farmerama/data/model/response/UserResponse.java
@@ -3,13 +3,13 @@ package com.example.farmerama.data.model.response;
 import com.example.farmerama.data.model.User;
 
 public class UserResponse {
-    private int id;
+    private int userId;
     private String name;
     private String email;
     private String password;
     private String role;
 
     public User getUser(){
-        return new User(id, name, email, password, role);
+        return new User(userId, name, email, password, role);
     }
 }
