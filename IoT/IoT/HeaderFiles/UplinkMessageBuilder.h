95d6af91c228e50bf26f29fb966bbae7b8e2bbad
Khaled Hammoun <kh.hammoun@gmail.com>
2022-05-29 10:50:18 +0200

    Seter for system error added. Will set all the validation bits to "Invalid"

--- IoT/IoT/HeaderFiles/UplinkMessageBuilder.h
+++ IoT/IoT/HeaderFiles/UplinkMessageBuilder.h
@@ -8,3 +8,4 @@ void uplinkMessageBuilder_setHumidityData(uint16_t data);
 void uplinkMessageBuilder_setTemperatureData(int16_t data);
 void uplinkMessageBuilder_setCO2Data(uint16_t data);
 void uplinkMessageBuilder_setSoundData(uint16_t data);
+void uplinkMessageBuilder_setSystemErrorState();
