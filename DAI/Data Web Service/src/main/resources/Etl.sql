7061fd96c8c9991020f10d3934a478b4093dd41c
Luis <lfponton@gmail.com>
2022-05-30 14:21:30 +0200

    update etl with transformations for measurements

--- DAI/Data Web Service/src/main/resources/Etl.sql
+++ DAI/Data Web Service/src/main/resources/Etl.sql
@@ -65,13 +65,43 @@ WHERE measured_date > (@LastLoadDate);
 ALTER TABLE [stage].[FactEnvironment]
     ADD CONSTRAINT FK_FactEnvironment_0 FOREIGN KEY (area_id) REFERENCES [stage].[DimArea] (area_id);
 
--- Transformation: because the devices can send data within the same minute, there will be duplicate key issues
+ -- Transformation: because the devices can send data within the same minute, there will be duplicate key issues
 -- in the dwh schema if the records with same time are not removed (at least one, but here we are deleting both)
-DELETE FROM dwh.stage.FactEnvironment
+DELETE FROM [stage].[FactEnvironment]
 WHERE CONCAT(measuredDate, '.' ,minute, '.', hour) in
-(SELECT DISTINCT CONCAT(measuredDate, '.' ,minute, '.', hour) FROM dwh.stage.FactEnvironment
+(SELECT CONCAT(measuredDate, '.' ,minute, '.', hour) FROM [stage].[FactEnvironment]
 GROUP BY CONCAT(measuredDate, '.' ,minute, '.', hour)
 HAVING COUNT(*) > 1)
+
+-- Transformation: Validate that each measurement is within the acceptable validation range.
+-- If not, substitute the measurement for the average of the measurements in the extracted table
+UPDATE [stage].[FactEnvironment]
+SET [temperature] =
+	(SELECT ROUND(AVG(temperature), 2)
+	FROM [stage].[FactEnvironment]
+	WHERE [temperature] BETWEEN -20 AND 60)
+WHERE [temperature] is null OR [temperature] NOT BETWEEN -20 AND 60;
+
+UPDATE [stage].[FactEnvironment]
+SET [humidity] =
+	(SELECT ROUND(AVG(humidity), 2)
+	FROM [stage].[FactEnvironment]
+	WHERE [humidity] BETWEEN 0 AND 100)
+WHERE [humidity] is null OR [humidity] NOT BETWEEN 0 AND 100;
+
+UPDATE [stage].[FactEnvironment]
+SET [co2] =
+	(SELECT ROUND(AVG(co2), 2)
+	FROM [stage].[FactEnvironment]
+	WHERE [co2] BETWEEN 50 AND 3000)
+WHERE [co2] is null OR [co2] NOT BETWEEN 50 AND 3000;
+
+UPDATE [stage].[FactEnvironment]
+SET [sound] =
+	(SELECT ROUND(AVG(sound), 2)
+	FROM [stage].[FactEnvironment]
+	WHERE [sound] BETWEEN 0 AND 150)
+WHERE [sound] is null OR [sound] NOT BETWEEN 0 AND 150;
 
 -- Incremental Load
 -- start get added
