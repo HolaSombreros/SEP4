d03536599402ff15a7d5b604120f443887ddfc53
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2022-05-31 16:30:44 +0200

    moved marker class

--- AND/app/src/main/java/com/example/farmerama/fragment/HistoricalMeasurementsFragment.java
+++ AND/app/src/main/java/com/example/farmerama/fragment/HistoricalMeasurementsFragment.java
@@ -1,17 +1,10 @@
 package com.example.farmerama.fragment;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,16 +14,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.farmerama.R;
 import com.example.farmerama.data.util.DateFormatter;
+import com.example.farmerama.fragment.view.TimeMarkerView;
 import com.example.farmerama.viewmodel.MeasurementsViewModel;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.MPPointF;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -101,46 +93,12 @@ public class HistoricalMeasurementsFragment extends Fragment {
                     lineChart.setMarker(tmv);
                 }
 
-                    @Override
-                    public void onNothingSelected() {
-                    }
-                });
+                @Override
+                public void onNothingSelected() {
+                }
+            });
 
         });
 
-    }
-
-    public class TimeMarkerView extends MarkerView {
-        private TextView date;
-
-        public TimeMarkerView(Context context, int layoutResource) {
-            super(context, layoutResource);
-            date = findViewById(R.id.markerDate_text);
-        }
-
-        @Override
-        public void setOffset(MPPointF offset) {
-            super.setOffset(new MPPointF(offset.x / 2, -offset.y));
-        }
-
-        public void refreshContent(String date) {
-            this.date.setText(date);
-        }
-
-        @Override
-        public void draw(Canvas canvas, float posX, float posY) {
-            WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-            Display display = windowManager.getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            int width = size.x;
-            int w = getWidth();
-            if ((width - posX - w) < w) {
-                posX -= w;
-            }
-            canvas.translate(posX, posY);
-            draw(canvas);
-            canvas.translate(-posX, -posY);
-        }
     }
 }
